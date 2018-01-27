30cd8490c3a7ddd5563c2774ba10d5865ef75733
lhfaguiar <lais.harumi.f@gmail.com>
2018-01-27 09:33:30 -0200

    raios e trovoes

---
+++ GetPlay.gd
@@ -0,0 +1,10 @@
+extends Button
+
+# class member variables go here, for example:
+# var a = 2
+# var b = "textvar"
+
+func _ready():
+#this event is called once per key-press
+    if event.is_action_pressed("PlayButton"):
+		get_node("TelaPrincipal")
